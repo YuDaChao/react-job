412d61eb63378aebb74000e3e0c13b68844af5db
happyu <290945266@qq.com>
2018-05-17 08:01:26 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Button } from 'antd-mobile'
+
+render(<Button type="primary">button</Button>, document.getElementById('root'))
